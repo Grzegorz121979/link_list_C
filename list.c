a7331a0dc055bf9ee1acb2fdb9cbb85c2c116dd9
Grzegorz121979 <pociechagrzegorz@outlook.com>
2024-02-07 19:50:12 +0100

    next commit

--- list.c
+++ list.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node
 {
     int number;
     struct node *next;
-} 
-node;
+} node;
 
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Hello, world\n");
+    node *list = NULL;
+
+    for (int i = 0; i < argc; i++)
+    {
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+        n->next = list;
+
+        list = n;
+    }
+
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
 
     return 0;
 }
